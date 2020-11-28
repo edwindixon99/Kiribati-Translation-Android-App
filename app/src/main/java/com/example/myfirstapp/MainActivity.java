c5e908c10c79c2bf2b57fc5ab1bebbf8ae71bf5b
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2020-11-28 20:01:43 +1300

    Made first App

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -11,6 +11,8 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
